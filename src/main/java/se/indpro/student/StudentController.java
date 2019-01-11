f3cc3aaffbeac2ab714c598a979b644cc7a62782
ardent1995 <ikbaltohosif321@gmail.com>
2019-01-11 15:02:46 +0530

    intermidiate commit

--- src/main/java/se/indpro/student/StudentController.java
+++ src/main/java/se/indpro/student/StudentController.java
@@ -2,23 +2,38 @@ package se.indpro.student;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class StudentController {
 //	@Autowired
 //	private StudentService studentService;
 //	
+	@PostMapping("/")
+	@ResponseBody
+	public String getAllStudents(HttpServletRequest request){
+		return "<h1>"+request.getParameter("name")+" "+request.getParameter("department")+"</h1>";
+	}
+	
 //	@GetMapping("/")
-//	public List<Student> getAllStudents(){
-//		
-//	}
+	@RequestMapping(value="/",method= RequestMethod.GET)
+	public ModelAndView getForm() {
+		ModelAndView modelandView = new ModelAndView();
+		modelandView.setViewName("hellotext");
+		return modelandView;
+	}
 //	
 //	@PostMapping("/")
 //	public void addStudent(@RequestBody Student student) {
